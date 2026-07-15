a7bd193303c229d30d7358a8d1c795056ad047c6
wa-maro <theo.pombe@gmail.com>
2026-07-15 18:08:05 +0300

    chore(web/setup): intialize the web application powered by angular

---
+++ web/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+
+@Component({
+  selector: 'app-root',
+  imports: [MatSlideToggle],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App {}
